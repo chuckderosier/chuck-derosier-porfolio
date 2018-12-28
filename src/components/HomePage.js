34959197bfc873f95d60efc411cce3d110e82b1b
chuckderosier <chuckderosier2018@gmail.com>
2018-12-28 11:32:57 -0500

    addedt title and styled it

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,19 +3,36 @@ import styled from 'styled-components'
 
 const HomePageStyle = styled.div`
     margin: 0 auto;
+    background-color: rgba(0,0,0,1);
     background-image: url("https://i.imgur.com/AKqYhT7.jpg");
     background-size: cover;
     background-repeat: none;
     width: 100vw;
     height: 100vh;
+    display: flex;
+    justify-content: center;
+    h1 {
+        text-align: center;
+        font-size: 2.5em;
+        padding: .5em;
+        background-color: rgba(0,0,0,.2);
+        border-radius: 25em;
+    }
+    .title {
+        width: 40vw;
+        height: 15vh;
+    }
 `
 
 class HomePage extends Component {
     render() {
         return (
             <div>
-                <h1>Chuck DeRosier's Portfolio</h1>
-                <HomePageStyle></HomePageStyle>
+                <HomePageStyle>
+                    <div className="title">
+                        <h1>Chuck DeRosier's Portfolio</h1>
+                    </div>
+                </HomePageStyle>
             </div>
         )
     }
