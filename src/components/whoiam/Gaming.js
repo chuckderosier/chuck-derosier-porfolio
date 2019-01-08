beab5aa5657bc40b7098083f61a7b2e8053da317
chuckderosier <chuckderosier2018@gmail.com>
2019-01-08 17:22:03 -0500

    started games page

--- src/components/whoiam/Gaming.js
+++ src/components/whoiam/Gaming.js
@@ -10,6 +10,8 @@ class Gaming extends Component {
         return (
             <GamingStyle>
                 <h1>Gaming</h1>
+                <h3> I am a bit old fashioned when it comes to games. I prefer board games over video games. When I say analog games you may think Monopoly or Clue, but the games I am referring to are far more complex and require strategy and planning.</h3>
+                <p>Examples of strategic games are Carcassonne, Citadels, Pandemic, RPG and many others. However, in more recent times I have mainly been playing games that my friend, Fred Noble, creates which range from simple fun games to complex strategy games. We are not just playing for fun but also play testing so we can troubleshoot game logic for balanced play. Below are some examples:</p>
             </GamingStyle>
         )
     }
