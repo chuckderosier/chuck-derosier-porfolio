bc23be3aab465f5a0b4571cca4990c16b96acf27
chuckderosier <chuckderosier2018@gmail.com>
2019-01-09 15:31:52 -0500

    basic style to gaming plus favicon

--- src/components/whoiam/Gaming.js
+++ src/components/whoiam/Gaming.js
@@ -4,6 +4,25 @@ import styled from 'styled-components'
 const GamingStyle = styled.div`
     background-color: rgba(0,0,0,1);
     color: rgba(255,255,0,1);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    h3 {
+        width: 70vw;
+        font-size: 1.5em;
+    }
+    p {
+        width: 70vw;
+        font-size: 1.2em;
+    }
+    img {
+        width: 15em;
+    }
+    a {
+        font-size: 2.5em;
+        color: rgba(255,255,255,1);
+        margin: 0 0 3em 0;
+    }
 `
 
 class Gaming extends Component {
@@ -11,8 +30,8 @@ class Gaming extends Component {
         return (
             <GamingStyle>
                 <h1>Gaming</h1>
-                <h3> I am a bit old fashioned when it comes to games. I prefer board games over video games. When I say analog games you may think Monopoly or Clue, but the games I am referring to are far more complex and require strategy and planning.</h3>
-                <p>Examples of strategic games are Carcassonne, Citadels, Pandemic, RPG and many others. However, in more recent times I have mainly been playing games that my friend, Fred Noble, creates which range from simple fun games to complex strategy games. We are not just playing for fun but also play testing so we can troubleshoot game logic for balanced play. Below are some examples:</p>
+                <p> I am a bit old fashioned when it comes to games. I prefer analog games over video games. When I say analog games you may think Monopoly or Clue, but the games I am referring to are far more complex and require strategy and planning.</p>
+                <p>Examples of strategic games are Carcassonne, Citadels, Pandemic, RPG and many others. However, in more recent times I have mainly been playing games that my friends, Fred Noble, Doug Powers and Ron Moore, have created which range from simple fun games to complex strategy games. We are not just playing for fun but also play testing so we can troubleshoot game logic for balanced play. Below are some examples:</p>
                 <div>
                     <h3>Political Divide and Conquer</h3>
                     <img src="https://i.imgur.com/ZMwmv1u.jpg" />
@@ -27,6 +46,7 @@ class Gaming extends Component {
                     <h3>For more information on these games and links to purchase click link below:</h3>
                     <a href="https://www.pangenre.com/">Pangenre, LLC</a>
                 </div>
+                <h1></h1>
             </GamingStyle>
         )
     }
